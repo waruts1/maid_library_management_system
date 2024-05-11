6e807a21fb8dbc5afcb7145c666d96261ec620ce
Collins Warutumo <collinswarutumo@gmail.com>
2024-05-11 12:59:21 +0300

    add intial commit

---
+++ src/main/java/org/example/maids_library_management_system/MaidsLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.maids_library_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidsLibraryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaidsLibraryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/maids_library_management_system/MaidsLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.maids_library_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaidsLibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
